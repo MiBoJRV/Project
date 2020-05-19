7829f5e77bdab54335b6c04229923cf0c706fec7
MiK <bodnar.mikhail@gmail.com>
2020-05-19 16:08:28 +0300

    Initial commit

---
+++ Gulp/pr/dev/static/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    svg4everybody({});
+});
+
+$('.slick-slider').slick({
+
+	//autoplay: true, 	 	//включить автоперемотку
+	//autoplaySpeed: 2000, 	//скорость перемотки 2сек
+	//arrows: true, 		 	// вкл/выкл стрелки перемотки
+	//fade: true 			 	// плавный переход картинок
+	//appendArrows: $()
+});
